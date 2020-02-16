9aa483e0ed6c32ef4b19b8153057602ad2f538fc
chc1129 <chc1129@gmail.com>
2020-02-16 15:07:11 +0900

    fix commit

---
+++ chap02/practice/gengo.py
@@ -0,0 +1,16 @@
+def gengo(year):
+	if year < 1868:
+		return ''
+	elif year < 1912:
+		return '明治' + str(year - 1867) + '年'
+	elif year < 1926:
+		return '大正' + str(year - 1911) + '年'
+	elif year < 1989:
+		return '昭和' + str(year - 1925) + '年'
+	elif year < 2019:
+		return '平成' + str(year - 1988) + '年'
+	else:
+		return '令和' + str(year - 2018) + '年'
+
+for i in range(1866, 2022):
+	print(str(i) + str(gengo(i)))
